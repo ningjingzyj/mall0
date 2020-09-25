c682120bcdc0d1c5918ab461e6c2fd4ad16d401b
ningjing <2953500363@qq.com>
2020-09-25 14:58:06 +0800

    目录结构设置

---
+++ src/pages/home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <nav-header></nav-header>
+        <router-view></router-view>
+        <nav-footer></nav-footer>
+    </div>
+</template>
+
+<script>
+    //import someComponent from './someComponent'
+    export default {
+        name: "",
+        data() {
+            return {
+                msg: "Hello Vue.js"
+            }
+        },
+        component: {
+            //someComponent
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
